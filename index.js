620747d5c182e4c879b2237c5cad5e775ea778e6
Brian Koehler <brian@koehler.ca>
2021-07-09 18:40:26 -0700

    update packages

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var io = require('socket.io')(server,{transports: ["websocket"]});
 var port = process.env.PORT || 3000;
-
-io.set('transports', ['websocket']);
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
